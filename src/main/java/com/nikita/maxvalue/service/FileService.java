4470281fc65119a064d090126b1cae0be3b15849
Nikita <n.maltsev@starfish24.com>
2025-02-05 15:37:48 +0300

    first commit

--- src/main/java/com/nikita/maxvalue/service/FileService.java
+++ src/main/java/com/nikita/maxvalue/service/FileService.java
@@ -1,6 +1,14 @@
 package com.nikita.maxvalue.service;
 
 public interface FileService {
-
+    /**
+     * Находит N-е максимальное число в файле
+     *
+     * @param filePath путь к файлу
+     * @param n позиция числа
+     * @return N-е максимальное число
+     * @throws IllegalArgumentException если n некорректно
+     * @throws RuntimeException если возникла ошибка при чтении файла
+     */
     Long findMaxNumber(String filePath, int n);
 }
